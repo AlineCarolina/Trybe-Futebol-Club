cb8d405775e3b38a0b2f7b27afd19629b44520a1
Aline <eualine2017@gmail.com>
2022-03-28 18:53:57 -0300

    requisito 11

--- app/backend/src/validations/loginValidation.ts
+++ app/backend/src/validations/loginValidation.ts
@@ -3,20 +3,19 @@ import { NextFunction, Request, Response } from 'express';
 const loginValidation = async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
+  if (!email) res.status(401).json({ message: 'All fields must be filled' });
+
   const regex = /\S+@\S+\.\S+/;
   const validEmail = regex.test(email);
 
-  if (!validEmail) {
-    return res.status(401).json({
-      message: 'Incorrect email or password',
-    });
-  }
+  if (!validEmail) res.status(401).json({ message: 'Incorrect email or password' });
 
   if (password.length < 6) {
-    return res.status(400).json({
+    res.status(400).json({
       message: 'Password must be at least 6 characters',
     });
   }
+
   next();
 };
 
